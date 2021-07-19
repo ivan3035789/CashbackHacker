c21ef3f35c611c60ecd91b828031609cf2a83299
ivan3035789 <82367614+ivan3035789@users.noreply.github.com>
2021-07-19 12:14:39 +0300

    added branch junit4-platform, tests junit4, tests junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,34 +9,62 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void iMustOfferToSpendAnother100IfTheAmountIs900() {
+    public void iMustOfferToSpendAnother100IfTheAmountIs900Junit4() {
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void youShouldOfferToSpendZeroIfTheAmountIs1000() {
+    public void youShouldOfferToSpendZeroIfTheAmountIs1000Junit4() {
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
 
     }
 
     @Test
-    public void iMustOfferToSpendAnother500IfTheAmountIs1500() {
+    public void iMustOfferToSpendAnother500IfTheAmountIs1500Junit4() {
         int amount = 1500;
 
         int actual = service.remain(amount);
         int expected = 500;
 
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void iMustOfferToSpendAnother100IfTheAmountIs900Junit5() {
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void youShouldOfferToSpendZeroIfTheAmountIs1000Junit5() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void iMustOfferToSpendAnother500IfTheAmountIs1500Junit5() {
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+        Assertions.assertEquals(expected, actual);
     }
 
 }
