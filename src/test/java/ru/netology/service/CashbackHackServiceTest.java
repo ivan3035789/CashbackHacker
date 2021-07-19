4853a99ccd21195c17dfbadb06b9715a37044c7b
ivan3035789 <82367614+ivan3035789@users.noreply.github.com>
2021-07-19 11:37:53 +0300

    added branch testng, tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void iMustOfferToSpendAnother100IfTheAmountIs900() {
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void youShouldOfferToSpendZeroIfTheAmountIs1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void iMustOfferToSpendAnother500IfTheAmountIs1500() {
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual,expected);
+    }
+
+}
